da98d18fe4fa5cf7444a4739f12dfc81056559e1
Diego <77720680+ninesky35@users.noreply.github.com>
2021-07-18 19:01:03 -0500

    Create calle.js

---
+++ src/calle.js
@@ -0,0 +1,11 @@
+let phrases = [
+	'Eh bigote, que te poné en ortiva. Todo piola con los pibitos, eh.',
+	'No tené nada pa tomar. Dale, no te pongá la gorra. Vamo a tomar algo con lo pibitos.',
+	'Eh guachín, haceme la segunda, loco, no me dejés tirado, está todo piola con vos, todo lindo.',
+	'Qué descanso guacho, eh qué me descansás, para descansar está la almohada.',
+	'Cosechala, juntala en pala y fumanchala.',
+	'Eh loco no tené una monedita, una monedita, pa los pibe que queremos tomar algo. Dale no te cortés.',
+	'Eh loco, rescatate o te rescato.',
+	'Larga las llantas.'
+];
+console.log(phrases[Math.floor(Math.random() * phrases.length)]);
